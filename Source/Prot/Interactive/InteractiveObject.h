897b09a593b9529cd3a575cce24b8bad78574e6d
UnoYakshi <YellowWanna@gmail.com>
2018-05-31 02:22:17 +0300

    Added InteractiveObject class (no functionality yet)...

---
+++ Source/Prot/Interactive/InteractiveObject.h
@@ -0,0 +1,25 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "InteractiveObject.generated.h"
+
+UCLASS()
+class PROT_API AInteractiveObject : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	AInteractiveObject();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:	
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
