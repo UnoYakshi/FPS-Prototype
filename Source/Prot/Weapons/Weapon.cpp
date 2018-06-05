d0a3ec58508dfb45f1c4c169b537e9b2efd2549a
UnoYakshi <YellowWanna@gmail.com>
2018-06-05 06:51:33 +0300

    Started to design and implement weapons...

---
+++ Source/Prot/Weapons/Weapon.cpp
@@ -0,0 +1,90 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Weapon.h"
+
+
+AWeapon::AWeapon()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AWeapon::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AWeapon::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AWeapon::Fire()
+{
+	if (ROLE == ROLE_Authority)
+	{
+		ServerFire();
+	}
+
+	AActor* WeaponOwner = GetOwner();
+	if (WeaponOwner)
+	{
+		FVector EyeLocation;
+		FRotator EyeRotation;
+
+		WeaponOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+		FVector ShotDirection = EyeRotation.Vector();
+		FVector TraceEnd = EyeLocation + (ShotDirection * 10000.f);
+
+		// Bullet spread...
+		float HalfRad = FMath::DegreesToRadians(WeaponData.BulletSpread);
+
+		FCollisionQueryParams QueryParams;
+		QueryParams.AddIgnoredActor(MyOwner);
+		QueryParams.AddIgnoredActor(this);
+		QueryParams.bTraceComplex = true;
+		QueryParams.bReturnPhysicalMaterial = true;
+
+		FVector TracerEndPoint = TraceEnd;
+		EPhysicalSurface SurfaceType = SurfaceType_Default;
+		FHitResult Hit;
+		if (GetWorld()->LineTraceSingleByChannel(Hit, EyeLocation, TraceEnd, COLLISION_WEAPON, QueryParams))
+		{
+			// Blocking hit! Process damage
+			AActor* HitActor = Hit.GetActor();
+
+			SurfaceType = UPhysicalMaterial::DetermineSurfaceType(Hit.PhysMaterial.Get());
+
+			float ActualDamage = BaseDamage;
+			if (SurfaceType == SURFACE_FLESHVULNERABLE)
+			{
+				ActualDamage *= 4.0f;
+			}
+
+			UGameplayStatics::ApplyPointDamage(HitActor, ActualDamage, ShotDirection, Hit, MyOwner->GetInstigatorController(), MyOwner, WeaponData.DamageType);
+
+			PlayImpactEffects(SurfaceType, Hit.ImpactPoint);
+
+			TracerEndPoint = Hit.ImpactPoint;
+		}
+
+		if (DebugWeaponDrawing > 0)
+		{
+			DrawDebugLine(GetWorld(), EyeLocation, TraceEnd, FColor::White, false, 1.0f, 0, 1.0f);
+		}
+
+		PlayFireEffects(TracerEndPoint);
+
+		if (Role == ROLE_Authority)
+		{
+			HitScanTrace.TraceTo = TracerEndPoint;
+			HitScanTrace.SurfaceType = SurfaceType;
+		}
+
+		LastFireTime = GetWorld()->TimeSeconds;
+	}
+	}
+}
+
