ec8d8acc2bf338d9517b3683bf3c40ad379d30d3
oninbo <oninbo@gmail.com>
2018-12-12 23:31:17 +0300

    added sphere mesh to Bot

--- Source/Prot/Bot.cpp
+++ Source/Prot/Bot.cpp
@@ -9,6 +9,16 @@ ABot::ABot()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	UStaticMeshComponent* SphereVisual = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("VisualRepresentation"));
+	SphereVisual->SetupAttachment(RootComponent);
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> SphereVisualAsset(TEXT("/Game/StarterContent/Shapes/Shape_Sphere.Shape_Sphere"));
+	if (SphereVisualAsset.Succeeded())
+	{
+		SphereVisual->SetStaticMesh(SphereVisualAsset.Object);
+		SphereVisual->SetRelativeLocation(FVector(0.0f, 0.0f, -40.0f));
+		SphereVisual->SetWorldScale3D(FVector(0.8f));
+	}
+
 }
 
 // Called when the game starts or when spawned
