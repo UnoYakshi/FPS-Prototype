d0ab3a69ab7102da440d998ceccc3cb2950a1b67
unknown <uno.yakshi@gmail.com>
2018-12-14 01:21:16 +0300

    Added build config...

--- Source/Prot/Prot.Build.cs
+++ Source/Prot/Prot.Build.cs
@@ -1,4 +1,4 @@
-// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 
@@ -7,7 +7,26 @@ public class Prot : ModuleRules
 	public Prot(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "NavigationSystem" });
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
-	}
+        // Uncomment if you are using Slate UI
+        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+        // Uncomment if you are using online features
+        // PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+        // To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+        // BEGIN STEAM INTEGRATION
+
+        // Uncomment if you are using online features
+        //PrivateDependencyModuleNames.Add("OnlineSubsystem");
+        //PrivateDependencyModuleNames.Add("OnlineSubsystemNull");
+        //if ((Target.Platform == UnrealTargetPlatform.Win32) || (Target.Platform == UnrealTargetPlatform.Win64) || (Target.Platform == UnrealTargetPlatform.Linux))
+        //{
+        //    DynamicallyLoadedModuleNames.Add("OnlineSubsystemSteam");
+        //}
+
+        // END STEAM INTEGRATION
+    }
 }
